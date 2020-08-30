c257edb27e877a32454defe5a8a20af654823ea8
learntodroid <56079350+learntodroid@users.noreply.github.com>
2020-08-30 15:13:22 +1000

    Initial commit

---
+++ app/src/main/java/com/learntodroid/navigationtutorial/Fragment1.java
@@ -0,0 +1,37 @@
+package com.learntodroid.navigationtutorial;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+public class Fragment1 extends Fragment {
+    Button navigateToFragment2Button;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_fragment1, container, false);
+
+        navigateToFragment2Button = view.findViewById(R.id.fragment_fragment1_navigateToFragment2);
+        navigateToFragment2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_fragment1_to_fragment2);
+            }
+        });
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/learntodroid/navigationtutorial/Fragment2.java
@@ -0,0 +1,25 @@
+package com.learntodroid.navigationtutorial;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Fragment2 extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_fragment2, container, false);
+
+        return view;
+    }
+}
